dd277c38a8ec69cd354c9ef97e25d9ddd8ea1edb
Sanjeev <sanjeev.neelakantan@chainsys.com>
2026-01-08 10:32:56 +0530

    test1

---
+++ stylelint.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  extends: ["stylelint-config-standard-scss"],
+  rules: {
+    "color-no-invalid-hex": true,
+    "block-no-empty": true,
+    "selector-max-id": 0,
+    "declaration-block-no-duplicate-properties": true,
+    "no-descending-specificity": null
+  }
+};
